1ac37a917cddbe5612f9048564a521a3bc90fba6
Pedro Aglailton <pedroaglailton@gmail.com>
2026-06-02 23:23:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -505,6 +505,7 @@ app.get('/', (_req, res) => {
 });
 app.get('/noc',     (_req, res) => res.sendFile(path.join(nocRoot, 'noc.html')));
 app.get('/tracker', (_req, res) => res.sendFile(path.join(nocRoot, 'tracker.html')));
+app.get('/fortaleza_roads.geojson', (_req, res) => res.sendFile(path.join(nocRoot, 'fortaleza_roads.geojson')));
 
 // ============================================================================
 // WebSocket
